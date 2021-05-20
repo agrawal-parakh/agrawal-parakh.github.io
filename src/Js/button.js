fd333d99a55b5cb208f6b8f0d923ddb44674b402
parakh <parakhagarwal945@gmail.com>
2021-05-21 01:10:23 +0530

    mobile view how it works updated

--- src/Js/button.js
+++ src/Js/button.js
@@ -1,32 +1,78 @@
-function show() {
-    // var a = document.querySelectorAll(".how-it-works-img-container")
-    // for (var i = 1; i < a.length; i++) {
-    //     a[i].style.display = "none";
-    // }
 
+function show2(){
     var a = document.querySelectorAll(".common-how-it-work-class");
-    for (var i = 0; i < a.length; i++) {
-        a[i].style.border = "0.1rem solid red";
-    }
-
-    var a = document.querySelectorAll(".common-how-it-work-class .number");
-    a.forEach((ele) => {
-        ele.style.fontSize = "1.5rem";
-        ele.style.color = "var(--bright-blue)";
-    })
-
-
-    var a = document.querySelectorAll(".common-how-it-work-class .text");
-    a.forEach((ele) => {
-        ele.style.fontSize = "1.3rem";
-        ele.style.color = "var(--dark-blue)";
-    })
+    a[1].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[1].style.fontSize = "1.5rem";
+    b[1].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[1].style.fontSize = "1.3rem";
+    c[1].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[1].style.display = "block";
 }
 
+function show3(){
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a[2].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[2].style.fontSize = "1.5rem";
+    b[2].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[2].style.fontSize = "1.3rem";
+    c[2].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[2].style.display = "block";
+}
 
-//works in all border when button click
-// var a=document.querySelectorAll(".common-how-it-work-class");
-// for(var i=0;i<a.length;i++){
-// a[i].style.border="0.1rem solid red";
-// }
+function show4(){
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a[3].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[3].style.fontSize = "1.5rem";
+    b[3].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[3].style.fontSize = "1.3rem";
+    c[3].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[3].style.display = "block";
+}
 
+function show5(){
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a[4].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[4].style.fontSize = "1.5rem";
+    b[4].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[4].style.fontSize = "1.3rem";
+    c[4].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[4].style.display = "block";
+}
+
+function show6(){
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a[5].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[5].style.fontSize = "1.5rem";
+    b[5].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[5].style.fontSize = "1.3rem";
+    c[5].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[5].style.display = "block";
+}
+
+function show7(){
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a[6].style.border = "0.1rem solid var(--bright-blue)";
+    var b= document.querySelectorAll(".common-how-it-work-class .number");
+    b[6].style.fontSize = "1.5rem";
+    b[6].style.color = "var(--bright-blue)";
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c[6].style.fontSize = "1.3rem";
+    c[6].style.color = "var(--dark-blue)";
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d[6].style.display = "block"
+}
