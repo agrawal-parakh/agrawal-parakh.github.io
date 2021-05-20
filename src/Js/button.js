fbe91bf76f21e0f2f0aa8da87ee487d18c3edab0
parakh <parakhagarwal945@gmail.com>
2021-05-21 02:07:36 +0530

    task04 dom work added

--- src/Js/button.js
+++ src/Js/button.js
@@ -1,78 +1,212 @@
-
-function show2(){
+function show1() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
+    a[0].style.border = "0.1rem solid var(--bright-blue)";
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
+    b[0].style.fontSize = "1.5rem";
+    b[0].style.color = "var(--bright-blue)";
+
+    var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
+    c[0].style.fontSize = "1.3rem";
+    c[0].style.color = "var(--dark-blue)";
+
+    var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
+    d[0].style.display = "block";
+}
+
+
+function show2() {
+    var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[1].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[1].style.fontSize = "1.5rem";
     b[1].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[1].style.fontSize = "1.3rem";
     c[1].style.color = "var(--dark-blue)";
+
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[1].style.display = "block";
 }
 
-function show3(){
+function show3() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[2].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[2].style.fontSize = "1.5rem";
     b[2].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[2].style.fontSize = "1.3rem";
     c[2].style.color = "var(--dark-blue)";
+   
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[2].style.display = "block";
+
 }
 
-function show4(){
+function show4() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[3].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[3].style.fontSize = "1.5rem";
     b[3].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[3].style.fontSize = "1.3rem";
     c[3].style.color = "var(--dark-blue)";
+
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[3].style.display = "block";
 }
 
-function show5(){
+function show5() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[4].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[4].style.fontSize = "1.5rem";
     b[4].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[4].style.fontSize = "1.3rem";
     c[4].style.color = "var(--dark-blue)";
+
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[4].style.display = "block";
 }
 
-function show6(){
+function show6() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[5].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[5].style.fontSize = "1.5rem";
     b[5].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[5].style.fontSize = "1.3rem";
     c[5].style.color = "var(--dark-blue)";
+
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[5].style.display = "block";
 }
 
-function show7(){
+function show7() {
     var a = document.querySelectorAll(".common-how-it-work-class");
+    a.forEach((ele) => {
+        ele.style.border = "0.1rem solid var(--dark-grey)"
+    })
     a[6].style.border = "0.1rem solid var(--bright-blue)";
-    var b= document.querySelectorAll(".common-how-it-work-class .number");
+
+    var b = document.querySelectorAll(".common-how-it-work-class .number");
+    b.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     b[6].style.fontSize = "1.5rem";
     b[6].style.color = "var(--bright-blue)";
+
     var c = document.querySelectorAll(".common-how-it-work-class .text");
+    c.forEach((ele) => {
+        ele.style.fontSize = "1.1rem";
+        ele.style.color = "var(--light-grey)"
+    })
     c[6].style.fontSize = "1.3rem";
     c[6].style.color = "var(--dark-blue)";
+
     var d = document.querySelectorAll(".how-it-works-img-container");
+    d.forEach((ele) => {
+        ele.style.display = "none";
+    })
     d[6].style.display = "block"
 }
+
